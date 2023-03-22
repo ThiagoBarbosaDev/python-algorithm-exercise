cd3fa4f8b35ababa3f42830e7f1f34b3efdaa26f
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-22 20:34:27 -0300

    feat: implement study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,26 @@
+def validate_student_presence(student_presence):
+    if student_presence[0] is None or student_presence[1] is None or type(student_presence[0]) is str or type(student_presence[1]) is str:
+        raise ValueError
+
+
+def is_target_time(student_presence, target_time):
+    validate_student_presence(student_presence)
+    most_people = 0
+    for student_hour in range(
+        student_presence[0], (student_presence[1] + 1)
+    ):
+        if student_hour == target_time:
+            most_people += 1
+    return most_people
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        most_people = 0
+        if target_time is None:
+            return None
+        for student_presence in permanence_period:
+            most_people += is_target_time(student_presence, target_time)
+        return most_people
+    except ValueError:
+        return None
