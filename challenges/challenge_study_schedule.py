56c232d1b8c8b5dd5b27d74674e21a3e864f873c
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-22 22:57:14 -0300

    fix: lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,17 @@
 def validate_student_presence(student_presence):
-    if student_presence[0] is None or student_presence[1] is None or type(student_presence[0]) is str or type(student_presence[1]) is str:
+    if (
+        student_presence[0] is None
+        or student_presence[1] is None
+        or type(student_presence[0]) is str
+        or type(student_presence[1]) is str
+    ):
         raise ValueError
 
 
 def is_target_time(student_presence, target_time):
     validate_student_presence(student_presence)
     most_people = 0
-    for student_hour in range(
-        student_presence[0], (student_presence[1] + 1)
-    ):
+    for student_hour in range(student_presence[0], (student_presence[1] + 1)):
         if student_hour == target_time:
             most_people += 1
     return most_people
