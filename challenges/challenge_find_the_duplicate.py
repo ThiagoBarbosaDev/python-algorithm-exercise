fe7caa470a29edaad700620d9525fe62cf705f2d
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-26 23:53:43 -0300

    feat: implement find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    unique_nums = set()
+    if not nums or len(set(nums)) == len(nums):
+        return False
+    for num in nums:
+        if num < 0:
+            return False
+        if num in unique_nums:
+            return num
+        unique_nums.add(num)
