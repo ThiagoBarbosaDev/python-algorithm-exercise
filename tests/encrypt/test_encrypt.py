e4ca0089430fb2ec77f9b14945a7111a3c0c01ca
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-27 01:20:08 -0300

    test(encrypt_message): implement unit test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('Vamo que vamo', 1) == 'V_omav euq oma'
+    assert encrypt_message('Vamo que vamo', 2) == 'omav euq om_aV'
+    assert encrypt_message('Vamo que vamo', 3) == 'maV_omav euq o'
+    assert encrypt_message('Vamo que vamo', 4) == 'omav euq _omaV'
+    assert encrypt_message('Vamo que vamo', 999) == 'omav euq omaV'
+    with pytest.raises(TypeError):
+        encrypt_message('Vamo que vamo', '999')
+    with pytest.raises(TypeError):
+        encrypt_message(None, 4)
